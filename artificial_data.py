6921dee3417a551cdbc1657b82d7910f92b54fc9
Ankush Gupta <84059577+ankushgpta2@users.noreply.github.com>
2022-02-15 14:54:10 -0500

    Generates Simulated V1 Neural Network Data
    Simulated data of V1 neural networks, consisting of biologically constrained network topology and spatiotemporal dynamics of activity related to stimulus presentation (drifting gratings), meant to closely mimic data acquired from experiments

--- artificial_data.py
+++ artificial_data.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 from dnn_structures import *
 import networkx as nx
 import collections
-from dnn_modules_for_sini import *
+from dnn_modules import *
 
 
 def artificial_main(artificial_parameters, general_parameters, autoencoder_parameters):
